d9ef26600aa58fd559818a84186dd731485b46ba
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-02-06 13:55:27 +0100

    Add a `Format` and `Lint` actions (#12)

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => setCount((c) => c + 1)}>
                     count is {count}
                 </button>
                 <p>
