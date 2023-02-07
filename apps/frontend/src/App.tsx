6486c12f8ec754fe486c6a81a3a00e69c32d7857
epoundor <freedausstanda@gmail.com>
2023-02-07 19:09:45 +0100

    feat:(front) creation of findAllReactComponent

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { Button } from '@react-site-editor/ui';
+
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Button
+                text="Click me"
+                onClick={() => alert('Button was clicked!')}
+                style={{
+                    backgroundColor: 'blue',
+                    color: 'white',
+                    padding: '10px 20px'
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,18 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { Button } from '@react-site-editor/ui';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <Button
-            text="Click me"
-            onClick={() => alert('Button was clicked!')}
-            style={{
-                backgroundColor: 'blue',
-                color: 'white',
-                padding: '10px 20px'
-            }}
-        />
         <App />
     </React.StrictMode>
 );
