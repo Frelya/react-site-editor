b0d6175ef3b5c1516875cdfd4283e2b92d2e5c3e
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-02-11 17:55:13 +0100

    style: format

--- packages/ui/tailwind.config.cjs
+++ packages/ui/tailwind.config.cjs
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,jsx,ts,tsx}"
-    ],
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {}
     },
