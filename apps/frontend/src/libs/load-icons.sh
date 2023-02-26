50ecf8ef1e3d9d296849b3b86ae8dae05add92da
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-02-26 18:48:30 +0100

    fix: add a little trick to fix pascalToKebab import

--- apps/frontend/src/libs/load-icons.sh
+++ apps/frontend/src/libs/load-icons.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
-BASE_DIR=./src/libs
-SCRIPT_NAME=getAvailableIcons
+if [ "$(pwd | awk -F/ '{print $NF}')" != "frontend" ]
+then
+  echo -e "\033[31mError: Please run this script from the 'apps/frontend' folder or the project root directory\033[39m \n"
+  exit 1
+fi
 
-SCRIPT_FILE=${BASE_DIR}/${SCRIPT_NAME}.ts
-GENERATED_JS=${BASE_DIR}/${SCRIPT_NAME}.js
-CONVERTED_CJS=${BASE_DIR}/${SCRIPT_NAME}.cjs
+BASE_DIR=./src/libs
+
+INTER_FILE=${BASE_DIR}/output
 OUTPUT_FILE=${BASE_DIR}/types/icons.type.ts
+SCRIPT_FILE=${BASE_DIR}/getAvailableIcons.ts
+
+REQUIRED_FILE=../../packages/functions/pascalToKebab.ts
+
+# Generate a single typescript file containing the pascalToKebab function
+
+# Step1: add the pascalToKebab function
+cat $REQUIRED_FILE > ${INTER_FILE}.ts
+
+# Step2: add a blank line
+echo >> ${INTER_FILE}.ts
+
+# Step3: add the getAvailableIcons script, by removing the import of
+# pascalToKebab at the top of the file
+tail -n +2 $SCRIPT_FILE >> ${INTER_FILE}.ts
 
 # transpile...
-tsc $SCRIPT_FILE &&
+tsc ${INTER_FILE}.ts &&
  # rename...
- cp $GENERATED_JS $CONVERTED_CJS &&
+ cp ${INTER_FILE}.js ${INTER_FILE}.cjs &&
   # run..
-  node $CONVERTED_CJS &&
+  node ${INTER_FILE}.cjs
    # clean...
-   rm $GENERATED_JS && rm $CONVERTED_CJS &&
+   rm ${INTER_FILE}* &&
      # format...
     npx eslint $OUTPUT_FILE --fix
+
+echo -e "\033[32m\nIcons types definition generated successfully !\033[39m"
