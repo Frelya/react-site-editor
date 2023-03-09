a5ccb5ec61271d562e86eff678f9d125aa819802
Eliot Fagnon <efagnon2002@gmail.com>
2023-03-09 19:12:17 +0100

    fix: add exposed components to tracking

---
+++ packages/ui/src/components/exposed/Button2/Button2.tsx
@@ -0,0 +1,34 @@
+import type {
+    ComponentChildren,
+    ComponentProp,
+    PredefinedComponentProps
+} from '@react-site-editor/types';
+import { PropsEnum } from '@react-site-editor/types';
+import ButtonStyle from './Button2.module.css';
+
+interface Button2Props {
+    text?: ComponentProp;
+    fontSize?: ComponentProp;
+    onClick?: () => void;
+    children?: ComponentChildren;
+}
+
+const Button2: React.FunctionComponent<Button2Props> = (props) => {
+    return (
+        <button
+            className={`${ButtonStyle.baseButton} ${ButtonStyle[`font-${props.fontSize?.value}`]}`}
+            onClick={props?.onClick}>
+            {props.text?.value}
+            {props.children}
+        </button>
+    );
+};
+
+export const defaultProps: PredefinedComponentProps<Button2Props> = {
+    text: { type: PropsEnum.TEXT, value: 'Button 2' },
+    fontSize: { type: PropsEnum.SIZE, value: '2' },
+    onClick: () => console.log('Button clicked'),
+    maxChildren: 2
+};
+
+export default Button2;
---
+++ packages/ui/src/components/exposed/Button3/Button3.tsx
@@ -0,0 +1,34 @@
+import type {
+    ComponentChildren,
+    ComponentProp,
+    PredefinedComponentProps
+} from '@react-site-editor/types';
+import { PropsEnum } from '@react-site-editor/types';
+import ButtonStyle from './Button3.module.css';
+
+interface Button3Props {
+    text?: ComponentProp;
+    fontSize?: ComponentProp;
+    onClick?: () => void;
+    children?: ComponentChildren;
+}
+
+const Button3: React.FunctionComponent<Button3Props> = (props) => {
+    return (
+        <button
+            className={`${ButtonStyle.baseButton} ${ButtonStyle[`font-${props.fontSize?.value}`]}`}
+            onClick={props?.onClick}>
+            {props.text?.value}
+            {props.children}
+        </button>
+    );
+};
+
+export const defaultProps: PredefinedComponentProps<Button3Props> = {
+    text: { type: PropsEnum.TEXT, value: 'Button 3' },
+    fontSize: { type: PropsEnum.SIZE, value: '2' },
+    onClick: () => console.log('Button clicked'),
+    maxChildren: 2
+};
+
+export default Button3;
