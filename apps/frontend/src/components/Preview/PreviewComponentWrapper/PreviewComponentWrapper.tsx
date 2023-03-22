a5389b03becbfa09e25fe136eac7698d06614008
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-03-22 02:11:29 +0100

    refactor: add the right icon for removal

--- apps/frontend/src/components/Preview/PreviewComponentWrapper/PreviewComponentWrapper.tsx
+++ apps/frontend/src/components/Preview/PreviewComponentWrapper/PreviewComponentWrapper.tsx
@@ -1,8 +1,8 @@
+import { useDispatch } from 'react-redux';
+import { deleteComponent } from '@/store/previewTree/previewTreeSlice';
 import PreviewDroppable from '@components/Preview/PreviewDroppable/PreviewDroppable';
 import Icon from '@components/Decorators/Icon';
 import PreviewComponentWrapperStyle from './PreviewComponentWrapper.module.css';
-import { useDispatch } from 'react-redux';
-import { deleteComponent } from '@/store/previewTree/previewTreeSlice';
 
 interface PreviewComponentWrapperProps {
     children: React.ReactNode | React.ReactNode[];
@@ -27,13 +27,6 @@ const PreviewComponentWrapper: React.FunctionComponent<PreviewComponentWrapperPr
             <div
                 className={PreviewComponentWrapperStyle.container + ' group'}
                 onClick={props.onClick}>
-                {/* <div className="absolute right-0 top-3/4 invisible group-hover:visible">
-                    <Icon
-                        name="add-circle-line"
-                        className=" rounded-full text-white fill-none bg-blue-500"
-                        description="Add component after"
-                    />
-                </div> */}
                 {/*
                     I have to stop bubble propagation,
                     That's why I don't use the onClick props of `Icon` 
@@ -43,10 +36,10 @@ const PreviewComponentWrapper: React.FunctionComponent<PreviewComponentWrapperPr
                         evt.stopPropagation();
                         handleDeleteElementClick(props.index);
                     }}
-                    className="absolute -right-3 -top-1/4 invisible group-hover:visible rotate-45">
+                    className="absolute -right-3 -top-1/4 h-6 w-6 invisible group-hover:visible">
                     <Icon
                         name="minus"
-                        className=" rounded-full text-white fill-none bg-blue-500"
+                        className="rounded-full bg-red-500 text-white"
                         description="Delete component"
                     />
                 </div>
