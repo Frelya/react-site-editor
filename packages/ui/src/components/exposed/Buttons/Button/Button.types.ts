f2efa7cb44e3d863826b78438aba68840466bdda
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-04-17 21:37:16 +0200

    feat: add a color property

--- packages/ui/src/components/exposed/Buttons/Button/Button.types.ts
+++ packages/ui/src/components/exposed/Buttons/Button/Button.types.ts
@@ -2,7 +2,9 @@ import type { ComponentChildren, ComponentProp } from '@react-site-editor/types'
 
 export interface ButtonProps {
     text: ComponentProp;
+    textColor: ComponentProp;
     fontSize: ComponentProp;
+    backgroundColor: ComponentProp;
     onClick: () => void;
     children?: ComponentChildren;
 }
