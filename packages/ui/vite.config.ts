571e500af573cc308195eaaf5c68d329dd4a4564
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-05-14 04:41:05 +0200

    feat: configure deployment

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -6,7 +6,7 @@ import { resolve } from 'path';
 export default defineConfig({
     plugins: [react()],
     build: {
-        target: 'esnext',
+        target: 'esnext'
     },
     resolve: {
         alias: [
