bfec3841abe7a7edd775f0a8b0e21d81c038532e
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-05-15 23:58:22 +0200

    ci: fix deployment action

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/react-site-editor',
     plugins: [react()],
     build: {
         target: 'esnext',
