f83f3163671e87662ab337f76ea570f75b073b35
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-05-16 00:55:30 +0200

    ci: fix deployment action

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         target: 'esnext',
-        outDir: '../../dist'
+        outDir: '../../'
     },
     resolve: {
         alias: [
