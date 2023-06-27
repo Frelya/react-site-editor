6831f35fa02239b59d2203c503f8e35cdda56be2
epoundor <freedausstanda@gmail.com>
2023-06-28 00:01:24 +0100

    feat(front): add pieces list

--- apps/frontend/src/components/ComponentsList/ComponentsList.tsx
+++ apps/frontend/src/components/ComponentsList/ComponentsList.tsx
@@ -3,8 +3,6 @@ import type { ComponentInfos } from '@react-site-editor/types';
 import SideBarSearchBar from '@components/SideBar/SideBarSearchBar';
 import SideBarTabTitle from '@components/SideBar/SideBarTabTitle';
 import ComponentsListGroups from '@components/ComponentsList/ComponentsListGroups';
-import Contextable from '../Decorators/Contexable';
-import { ContextMenuAction } from '@/types/contextMenu';
 
 interface ComponentsListProps {
     elements: ComponentInfos[];
@@ -13,52 +11,22 @@ interface ComponentsListProps {
 const ComponentsList: React.FunctionComponent<ComponentsListProps> = (props) => {
     const [searchQuery, setSearchQuery] = useState<string>('');
 
-    const actions: ContextMenuAction[] = [
-        {
-            label: 'SideBarAction A',
-            action: () => {
-                console.log('SideBarAction A');
-            }
-        },
-        {
-            label: 'SideBarAction B',
-            action: () => {
-                console.log('SideBarAction B');
-            }
-        },
-        {
-            label: 'SideBarAction C',
-            action: () => {
-                console.log('SideBarAction C');
-            }
-        }
-    ];
-    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const query = event.target.value;
-        setSearchQuery(query);
-    };
-
     return (
-        <Contextable actions={actions} className={styleClasses.container}>
-            <>
-                <div className={styleClasses.searchBar}>
-                    <input
-                        className={styleClasses.searchBarInput}
-                        type={'text'}
-                        placeholder={'Search components'}
-                        value={searchQuery}
-                        onChange={handleSearchChange}
-                    />
-                    <Icon name={'search'} className={'text-gray-600'} />
-                </div>
-                <p className={styleClasses.componentsListTitle}>
-                    {searchQuery.length > 0
+        <div className={styleClasses.container}>
+            <SideBarSearchBar
+                placeholder={'Search components'}
+                query={searchQuery}
+                setQuery={setSearchQuery}
+            />
+            <SideBarTabTitle
+                title={
+                    searchQuery.length > 0
                         ? `Search results for "${searchQuery}"`
-                        : 'Available components'}
-                </p>
-                <ComponentsListGroups elements={props.elements} filter={searchQuery} />
-            </>
-        </Contextable>
+                        : 'Available components'
+                }
+            />
+            <ComponentsListGroups elements={props.elements} filter={searchQuery} />
+        </div>
     );
 };
 
