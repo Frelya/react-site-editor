3d4705b853675fe090d2b744f8111c41ef2fa163
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-07-25 11:29:31 +0100

    ci: add Vercel config file

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -2,10 +2,9 @@
 
 set -x
 
-UPSTREAM_USER="private-frelya-team"
-UPSTREAM_REPO="https://github.com/$UPSTREAM_USER/react-site-editor-deploy.git"
+UPSTREAM_REPO="https://github.com/${UPSTREAM_USER}/${UPSTREAM_REPO_NAME}.git"
 UPSTREAM_BRANCH="main"
-DOWNSTREAM_BRANCH="yacine/deploy"
+DOWNSTREAM_BRANCH="main"
 
 echo "UPSTREAM_REPO=$UPSTREAM_REPO"
 
@@ -16,14 +15,14 @@ git config user.name "$UPSTREAM_USER"
 git config user.email "$UPSTREAM_USER@users.noreply.github.com"
 git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-git remote set-url origin "https://$UPSTREAM_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY_URL"
+git remote set-url origin "https://${UPSTREAM_USER}:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY_URL"
 git remote add upstream "$UPSTREAM_REPO"
 git fetch upstream 
 git remote -v
 
 git checkout "$DOWNSTREAM_BRANCH"
 
-MERGE_RESULT=$(git merge "upstream/$DOWNSTREAM_BRANCH")
+MERGE_RESULT=$(git merge "upstream/$UPSTREAM_BRANCH")
 if [[ $MERGE_RESULT != *"Already up to date."* ]]; then
   git commit -m "Merged upstream"  
   git push origin "$UPSTREAM_BRANCH"
