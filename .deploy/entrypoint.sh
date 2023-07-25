d9a74e10672e16214e70173c7d46c8323b96530b
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-07-25 11:09:00 +0100

    ci: add Vercel config file

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -23,7 +23,7 @@ git remote -v
 
 git checkout "$DOWNSTREAM_BRANCH"
 
-MERGE_RESULT=$(git merge "upstream/$UPSTREAM_BRANCH")
+MERGE_RESULT=$(git merge "upstream/$DOWNSTREAM_BRANCH")
 if [[ $MERGE_RESULT != *"Already up to date."* ]]; then
   git commit -m "Merged upstream"  
   git push origin "$UPSTREAM_BRANCH"
