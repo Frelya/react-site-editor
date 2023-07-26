f74350cf7ae51a94cda61a35a43a87cee7ea9e0e
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-07-27 01:34:30 +0200

    Setup the backend deployment (#122)

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -11,21 +11,22 @@ echo "UPSTREAM_REPO=$UPSTREAM_REPO"
 git clone "https://github.com/${GITHUB_REPOSITORY}.git" work
 cd work || { echo "Missing work dir" && exit 2 ; }
 
-git config user.name "$UPSTREAM_USER"
-git config user.email "$UPSTREAM_USER@users.noreply.github.com"
+git config user.name "${UPSTREAM_USER}"
+git config user.email "${UPSTREAM_USER}@users.noreply.github.com"
 git remote set-url origin "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-git remote set-url origin "https://${UPSTREAM_USER}:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY_URL"
+git remote set-url origin "https://${UPSTREAM_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY_URL}"
 git remote add upstream "$UPSTREAM_REPO"
 git fetch upstream 
 git remote -v
 
 git checkout "$DOWNSTREAM_BRANCH"
 
-MERGE_RESULT=$(git merge "upstream/$UPSTREAM_BRANCH")
-if [[ $MERGE_RESULT != *"Already up to date."* ]]; then
-  git commit -m "Merged upstream"  
-  git push origin "$UPSTREAM_BRANCH"
+REBASE_RESULT=$(git rebase "upstream/$UPSTREAM_BRANCH")
+
+if [[ $REBASE_RESULT != *"Already up to date."* ]]; then
+  git commit -m "Rebased upstream"  
+  git push origin "$DOWNSTREAM_BRANCH"
 fi
 
 cd ..
