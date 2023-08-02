203c1b2c7db4e00ac8bc0dd8c3235b5432749eae
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-08-02 04:35:59 +0100

    .

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         target: 'esnext',
-        outDir: './dist',
+        outDir: './dist'
     },
     resolve: {
         alias: [
