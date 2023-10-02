98d6583564aa665e7c7937ce991b2f4de300852f
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-10-02 02:14:16 +0200

    refactor: make the extractTokenFromCookies method private to AuthGuard

--- apps/backend/src/utils/token-from-cookies.ts
+++
@@ -1,8 +0,0 @@
-import type { Request } from 'express';
-
-import { TOKEN_KEY } from '@shared/constants';
-import { Token } from '@shared/token';
-
-export function extractTokenFromCookies(request: Request): Token.AccessToken | undefined {
-    return request.cookies[TOKEN_KEY];
-}
